a8326102edcd000b560034c46a22bfc106090f4d
BangKartavya <bangkartavya@gmail.com>
2025-01-17 09:40:48 +0530

    form finished

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import {type ClassValue, clsx} from "clsx";
 import qs from "query-string";
 import {twMerge} from "tailwind-merge";
+import {z} from "zod";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -191,3 +192,20 @@ export const getTransactionStatus = (date: Date) => {
 
 	return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+	// Sign Up
+
+	firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+	lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+	address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+	state: type === "sign-in" ? z.string().optional() : z.string().min(3).max(20),
+	postalcode: type === "sign-in" ? z.string().optional() : z.string().min(6).max(6),
+	dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(10).max(10),
+	aadhar: type === "sign-in" ? z.string().optional() : z.string().min(12).max(12),
+	city: type === "sign-in" ? z.string().optional() : z.string().min(3).max(20),
+	// common
+
+	email: z.string().email({message: "Not a valid email"}),
+	password: z.string().min(8, {message: "Password must be 8 characters or more"}),
+});
