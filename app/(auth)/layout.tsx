5da993e245e545927ed492f36a1620f808028929
BangKartavya <bangkartavya@gmail.com>
2025-01-28 18:03:44 +0530

    updated layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 import Image from "next/image";
 
-const AuthLayout = () => ({
-							  children,
-						  }: Readonly<{
-	children: React.ReactNode;
-}>) => (
-	<main className="flex min-h-screen w-full justify-between font-inter">
-		{children}
-		<div className="auth-asset">
-			<div>
-				<Image
-					src="/icons/auth-image.svg"
-					alt="Auth Image"
-					width={500}
-					height={500}
-				/>
+export default function AuthLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+	return (
+		<main className="flex min-h-screen w-full justify-between font-inter">
+			{children}
+			<div className="auth-asset">
+				<div>
+					<Image
+						src="/icons/auth-image.svg"
+						alt="Auth Image"
+						width={500}
+						height={500}
+					/>
+				</div>
 			</div>
-		</div>
-	</main>
-);
-
-export default AuthLayout;
+		</main>
+	)
+};
